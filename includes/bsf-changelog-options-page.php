64681be82b6a7d7d533e72a9140c7d065f023186
Abhijit Babasaheb Nage <abhijitn@bsf.io>
2021-03-10 16:30:26 +0530

    remove phpcs ignore

--- includes/bsf-changelog-options-page.php
+++ includes/bsf-changelog-options-page.php
@@ -23,7 +23,7 @@ defined( 'ABSPATH' ) || die( 'No direct script access allowed!' );
 							<td>
 								<?php
 								$bsf_changelog_category_template = get_option( 'bsf_changelog_category_template' );
-								if ( isset( $bsf_changelog_category_template ) && 'yes' === $bsf_changelog_category_template ) {//PHPCS:ignore:WordPress.PHP.StrictComparisons.LooseComparison
+								if ( isset( $bsf_changelog_category_template ) && 'yes' === $bsf_changelog_category_template ) {
 									echo '<input type="checkbox" checked name="bsf_changelog_category_template" value="yes">';
 								} else {
 									echo '<input type="checkbox" name="bsf_changelog_category_template" value="yes">';
